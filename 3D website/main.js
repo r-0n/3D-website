f7cd872f11edd861e7168ae9259bc4cf01a7ebfe
r-0n <ronywajah911@gmail.com>
2024-06-09 18:58:48 +0000

    update
    will add more personalized effects and features. but everything seems to be working

--- 3D website/main.js
+++ 3D website/main.js
@@ -19,11 +19,35 @@ camera.position.setZ(30);
 renderer.render(scene, camera)
 
 
+//adding torus
+
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus)
+
+
+//adding starlike structures 
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({ color: 0xFDB813 });
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3)
+        .fill()
+        .map(() => THREE.MathUtils.randFloatSpread(100));
+
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+
+//space background
+//const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+//scene.background = spaceTexture;
 
 //adding lightening
 const pointLight = new THREE.PointLight(0xffffff);
@@ -39,6 +63,55 @@ const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
+
+
+//about texture
+const ronTexture = new THREE.TextureLoader().load('about.jpg');
+
+const ron = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: ronTexture }));
+
+scene.add(ron);
+
+
+// adding Moon
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial({
+        map: moonTexture,
+        normalMap: normalTexture,
+    })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+ron.position.z = -5;
+ron.position.x = 2;
+
+//scroll effect
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    moon.rotation.x += 0.05;
+    moon.rotation.y += 0.075;
+    moon.rotation.z += 0.05;
+
+    ron.rotation.y += 0.01;
+    ron.rotation.z += 0.01;
+
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 //animation loop
 function animate() {
